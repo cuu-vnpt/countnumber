44d7ad29a9499666c44a6cc8376368d045e64e84
cuu-vnpt <kunnobita97@gmail.com>
2024-01-03 08:15:53 +0700

    new file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] s = {"c", "h", "a", "o"};
+        Console.WriteLine("Nhap vao ki tu ban phim");
+        string inputS = Console.ReadLine();
+        int count = 0; 
+        for (int i = 0; i < s.Length; i++)
+        {
+            if(s[i] == inputS)
+            {
+                count += 1;
+            }
+        }
+        Console.WriteLine("So ky tu co trong chuoi la: " + count);
+    }
+}
